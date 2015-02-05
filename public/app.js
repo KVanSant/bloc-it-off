43db01c3e60df3349ebf65debf9e86413b667482
Davis Van Sant <davisvansant@thelostwayfarer-imac.local>
2015-02-05 14:52:25 -0800

    Tasks can be marked as complete and can be seen in the completed view

--- public/app.js
+++ public/app.js
@@ -1,39 +1,74 @@
 blocitoff = angular.module('Blocitoff', ['firebase', 'ui.router']);
 
+
 blocitoff.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
 
-  $stateProvider.state('home', {
+  $stateProvider.state('active', {
     url: '/',
-    controller: 'Home.controller',
-    templateUrl: '/templates/home.html'
+    controller: 'Active.controller',
+    templateUrl: '/templates/active.html'
   });
+
+  $stateProvider.state('completed', {
+    url: '/completed',
+    controller: 'Completed.controller',
+    templateUrl: '/templates/completed.html'
+  });
+
+  $stateProvider.state('expired', {
+    url:'/expired',
+    controller: 'Expired.controller',
+    templateUrl: '/templates/expired.html'
+  })
 
 }]);
 
 
-blocitoff.controller('Home.controller', ['$scope', '$firebase', function($scope, $firebase) {
+blocitoff.controller('Active.controller', ['$scope', '$firebase',  function($scope, $firebase) {
   var ref = new Firebase("https://sweltering-heat-4642.firebaseio.com/tasks");
   var sync = $firebase(ref);
 
 //Sync task list as array
-  $scope.tasks = sync.$asArray();
+ var tasks = sync.$asArray();
+ $scope.tasks = tasks;
 
 
 //add task and add to firebase
-  $scope.addTask = function(task){
-  
-    $scope.tasks.$add({task: task});
-
+  $scope.addTask = function(task){ 
+    $scope.tasks.$add({task: task, state: "active", dateAdded: Firebase.ServerValue.TIMESTAMP });
     $scope.newTaskItem = "";
   };
 
 
-
-
-
-  
+  $scope.completeTask = function(taskId) {
+    var task = tasks.$getRecord(taskId);
+    task.state = "complete";
+    tasks.$save(task);
+  };
 
 }]);
+
+blocitoff.controller('Completed.controller', ['$scope', '$firebase', function($scope, $firebase) {
+  var ref = new Firebase("https://sweltering-heat-4642.firebaseio.com/tasks");
+  var sync = $firebase(ref);
+
+  //Sync task list as array
+ var tasks = sync.$asArray();
+ $scope.tasks = tasks;
+
+}]);
+
+
+blocitoff.controller('Expired.controller', ['$scope', '$firebase', function($scope, $firebase) {
+  var ref = new Firebase("https://sweltering-heat-4642.firebaseio.com/tasks");
+  var sync = $firebase(ref);
+
+//Sync task list as array
+ var tasks = sync.$asArray();
+ $scope.tasks = tasks;
+
+}]);
+
 
 
